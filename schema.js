80a93a27a52c4034ff61f2d1d007db0d83f181d0
Hasan <retiredlawyyer@gmail.com>
2021-10-22 01:22:16 +0300

    schema and root query type created for get methods about products and brands

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
   graphql,
   GraphQLSchema,
@@ -5,6 +6,7 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLInt,
+  GraphQLList,
   GraphQLNonNull
 } = require('graphql');
 
@@ -27,3 +29,43 @@ const BrandType = new GraphQLObjectType({
     })
 });
 
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: {
+        product: {
+            type: ProductType,
+            args: { 
+                id: { type: new GraphQLNonNull(GraphQLID) } 
+            },
+            resolve(parent, args) {
+                return axios.get('http://localhost:3000/products/' + args.id).then(res => res.data)
+            }
+        },
+        products: {
+            type: GraphQLList(ProductType),
+            resolve() {
+                return axios.get('http://localhost:3000/products').then(res => res.data)
+            }
+        },
+        brand: {
+            type: BrandType,
+            args: { 
+                id: { type: new GraphQLNonNull(GraphQLID) } 
+            },
+            resolve(parent, args) {
+                return axios.get('http://localhost:3000/brands/' + args.id).then(res => res.data)
+            }
+        },
+        brands: {
+            type: GraphQLList(BrandType),
+            resolve() {
+                return axios.get('http://localhost:3000/brands').then(res => res.data)
+            }
+        },
+    }
+})
+
+const MyGraphQLSchema = new GraphQLSchema({
+    query: RootQuery,
+})
+module.exports = MyGraphQLSchema
