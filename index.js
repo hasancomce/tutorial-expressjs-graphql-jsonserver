f9013b8320259109d282be5969fdf6c2397d77cd
Hasan <retiredlawyyer@gmail.com>
2021-10-22 01:16:50 +0300

    graphql middleware created and schema imported.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const MyGraphQLSchema = require('./schema');
 
 const app = express();
 
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: MyGraphQLSchema,
+    graphiql: true,
+  }),
+);
+ 
 app.listen(4000, () => console.log("Server is running at port 4000"));
