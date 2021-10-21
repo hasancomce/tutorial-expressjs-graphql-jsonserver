766c3ecc30bee8624a1191c0e8c4fe5f8fcf9b3b
Hasan <retiredlawyyer@gmail.com>
2021-10-22 01:23:47 +0300

    mutation type created for add, update and delete methods about products and brands with axios

--- schema.js
+++ schema.js
@@ -65,7 +65,88 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        productAdd: {
+            type: ProductType,
+            args: { 
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                title: { type: new GraphQLNonNull(GraphQLString) },
+                price: { type: new GraphQLNonNull(GraphQLInt) },
+                unitsInStock: { type: new GraphQLNonNull(GraphQLInt) },
+            },
+            resolve(parent, args) {
+                return axios.post('http://localhost:3000/products', {
+                    id: args.id,
+                    title: args.title,
+                    price: args.price,
+                    unitsInStock: args.unitsInStock,
+                }).then(res => res.data)
+            }
+        },
+        productUpdate: {
+            type: ProductType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                title: { type: GraphQLString },
+                price: { type: GraphQLInt },
+                unitsInStock: { type: GraphQLInt },
+            },
+            resolve(parent, args) {
+                return axios.patch('http://localhost:3000/products/' + args.id, args).then(res => res.data)
+            }
+        },
+        productDelete: {
+            type: ProductType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) }
+            },
+            resolve(parent, args) {
+                return axios.delete('http://localhost:3000/products/' + args.id).then(res => res.data)
+            }
+        },
+        brandAdd: {
+            type: BrandType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                title: { type: new GraphQLNonNull(GraphQLString) },
+                homeland: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            resolve(parent, args) {
+                return axios.post('http://localhost:3000/brands', {
+                    id: args.id,
+                    title: args.title,
+                    homeland: args.homeland,
+                }).then(res => res.data)
+            }
+        },
+        brandUpdate: {
+            type: BrandType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+                title: { type: GraphQLString },
+                homeland: { type: GraphQLString },
+            },
+            resolve(parent, args) {
+                return axios.patch('http://localhost:3000/brands/' + args.id, args).then(res => res.data)
+            }
+        },
+        brandDelete: {
+            type: BrandType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLID) },
+            },
+            resolve(parent, args) {
+                return axios.delete('http://localhost:3000/brands/' + args.id).then(res => res.data)
+            }
+        }
+    }
+})
+
 const MyGraphQLSchema = new GraphQLSchema({
     query: RootQuery,
+    mutation: Mutation,
 })
+
 module.exports = MyGraphQLSchema
